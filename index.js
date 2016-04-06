18d46c2557575629634fc4db80d542149366871a
backToNature <backtonature@foxmail.com>
2016-04-07 00:35:42 +0800

    update

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ $(function () {
     $('.header-tabs').each(function(){
         $(this).swiper({
             slidesPerView:'auto',
-            offsetPxBefore:25,
-            offsetPxAfter:10,
             calculateHeight: true
         })
     });
