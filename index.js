1d780e654a99d7a24324dfb788640380ac3778c5
backToNature <backtonature@foxmail.com>
2016-04-09 03:15:49 +0800

    update

--- index.js
+++ index.js
@@ -5,4 +5,29 @@ $(function () {
             calculateHeight: true
         })
     });
+    $(window).on('resize', function () {
+        $('.content').css('height', $(window).height() - 88);
+    });
+    $('.content').css('height', $(window).height() - 88);
+
+    $('body').on('touchmove', function(e) { 
+        e.preventDefault(); 
+    });
+    $('.scroll').on('touchmove', function (e) {
+        e.stopPropagation();
+    });
+
+    $('.list-item').on('touchstart', function () {
+        $(this).addClass('touchHighLight');
+        $(this).prev().css('border-color', '#f7f7f7');
+        $(this).one('touchmove.highLight', function () {
+            $(this).removeClass('touchHighLight');
+            $(this).off('touchend.highLight');
+        });
+        $(this).one('touchend.highLight', function () {
+            $(this).removeClass('touchHighLight');
+            $(this).off('touchmove.highLight');
+        });
+    });
+
 });
