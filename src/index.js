39e655ce98eeeec29ef6ecbe6c7a394fd0219703
郭豪(技术中心) <haoguo@sohu-inc.com>
2016-04-25 19:54:34 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 $(function () {
-    
+    var adaptive = function () {
+        $('.content').css('max-height', ($(window).height() - $('.header').height()) + 'px');
+    };
+    adaptive();
+    window.index_adaptive = adaptive;
+    $(window).on('resize', function () {
+        adaptive();
+    });
+
+    $('.list-item').on('tap', function (e) {
+        
+    });
+
+    (function () {
+        $('.header-tabs-ul').delegate('li', 'tap', function (e) {
+            var $this = $(this);
+            $('.header-tabs-ul li').removeClass('active');
+            $this.addClass('active');
+        });
+    }());
 });
